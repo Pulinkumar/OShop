0e494f3acd7120d52f08905275bbb00939ca03f1
Pulinkumar <pulin.patel.it@gmail.com>
2019-02-18 13:14:13 -0500

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyC_gtADHK2U7xBr9ZAEa3v7S_KQa7Cm3bc",
-    authDomain: "api-project-580966160758.firebaseapp.com",
-    databaseURL: "https://api-project-580966160758.firebaseio.com",
-    projectId: "api-project-580966160758",
-    storageBucket: "api-project-580966160758.appspot.com",
-    messagingSenderId: "580966160758"
-  }
+  production: false
 };
