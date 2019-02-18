1ce0a0a81fd20e0624a4f625ee746d3b893189e7
Pulinkumar <Pulinkumar@users.noreply.github.com>
2019-02-18 13:11:02 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyC_gtADHK2U7xBr9ZAEa3v7S_KQa7Cm3bc",
-    authDomain: "api-project-580966160758.firebaseapp.com",
-    databaseURL: "https://api-project-580966160758.firebaseio.com",
-    projectId: "api-project-580966160758",
-    storageBucket: "api-project-580966160758.appspot.com",
-    messagingSenderId: "580966160758"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
